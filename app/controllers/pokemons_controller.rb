05d693c25472c049af77e1b7c1a25fbdfbd5de62
lamckenneth <lam.c.kenneth@berkeley.edu>
2015-04-02 18:26:04 -0700

    I am a POKEMON MASTER!!!

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -11,22 +11,22 @@ class PokemonsController < ApplicationController
     @pokemon_kill = Pokemon.find(params[:id])
     @pokemon_kill.health -= 10
     if @pokemon_kill.health <= 0
-  	  @pokemon_kill.destroy
-  	else
-  	  @pokemon_kill.save!
-  	end
-  	redirect_to trainer_path(id: current_trainer.id)
+      @pokemon_kill.destroy
+    else
+      @pokemon_kill.save!
+    end
+    redirect_to trainer_path(id: current_trainer.id)
   end
 
   def new
-  	@pokemon = Pokemon.new()
+    @pokemon = Pokemon.new()
   end
 
   def create
-  	@pokemon = params[:pokemon]
-  	@name = @pokemon["name"]
-  	@error = Pokemon.create(:name => @name, :level => 1, :health => 100, :trainer_id => current_trainer.id)
-  	if (@error.errors.full_messages.to_sentence.empty?)
+    @pokemon = params[:pokemon]
+    @name = @pokemon["name"]
+    @error = Pokemon.create(:name => @name, :level => 1, :health => 100, :trainer_id => current_trainer.id)
+    if (@error.errors.full_messages.to_sentence.empty?)
       redirect_to trainer_path(id: current_trainer.id)
     else
       flash[:error] = @error.errors.full_messages.to_sentence
